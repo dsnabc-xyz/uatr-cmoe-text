f14ce51988d76226830340b8cb9cfbfe06f4d94e
dsnabc-xyz <dingshuni43@gmail.com@example.com>
2026-04-26 16:07:36 +0800

    update deepship mel baseline config

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from macls.utils.utils import add_arguments, print_arguments
 
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
-add_arg('configs',              str,    'configs/cam++.yml',        '配置文件')
+add_arg('configs',              str,    'configs//deepship/resnet_ap_ce.yml',        '配置文件')
 add_arg('data_augment_configs', str,    'configs/augmentation.yml', '数据增强配置文件')
 add_arg("local_rank",           int,    0,                          '多卡训练需要的参数')
 add_arg("use_gpu",              bool,   True,                       '是否使用GPU训练')
@@ -21,7 +21,7 @@ print_arguments(args=args)
 # 获取训练器
 trainer = MAClsTrainer(configs=args.configs,
                        use_gpu=args.use_gpu,
-                       data_augment_configs=args.data_augment_configs,
+                       data_augment_configs=None,
                        overwrites=args.overwrites)
 
 trainer.train(save_model_path=args.save_model_path,
